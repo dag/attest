fb362f5c1c4e3ac847730c42aa32c680e95706b0
Dag Odenhall <dag.odenhall@gmail.com>
2010-11-11 17:52:29 +0100

    mathtests -> sampletests

--- sampletests.py
+++ sampletests.py
@@ -1,28 +1,28 @@
 from schluck import Tests, Assert
 import time
 
-math = Tests()
+sample = Tests()
 
-@math.test
+@sample.test
 def arithmetics():
     """Test that basic arithmetics don't make sense."""
     time.sleep(.1)
     Assert(1 + 1) == 3
 
-@math.test
+@sample.test
 def compare():
     time.sleep(.1)
     Assert(5) < 10
 
-@math.test
+@sample.test
 def contains():
     time.sleep(.1)
-    2 in Assert([1,2,3])
+    5 in Assert([1, 2, 3])
 
 for x in xrange(17):
-    @math.test
+    @sample.test
     def passing():
         time.sleep(.1)
 
 if __name__ == '__main__':
-    math.run()
+    sample.run()
