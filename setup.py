cc1cf418d87d4c69de36e86947a7704cb262d69d
Dag Odenhall <dag.odenhall@gmail.com>
2010-11-27 21:40:05 +0100

    Release: 0.3

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ from setuptools import setup
 
 setup(
     name='Attest',
-    version='0.3dev',
+    version='0.3',
     description='Modern, Pythonic unit testing.',
     long_description=__doc__,
 
