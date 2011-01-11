e0376aee60ac2300b6c1e23fccc0123c2b6b0964
Dag Odenhall <dag.odenhall@gmail.com>
2011-01-11 10:30:37 +0100

    Version is now 0.5dev

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if sys.version_info[0] == 2:
 
 setup(
     name='Attest',
-    version='0.4',
+    version='0.5',
     description='Modern, Pythonic unit testing.',
     long_description=__doc__,
 
