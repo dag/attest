f1a7cdd29e6c5164d4a71fc23adae15fc05d3056
Dag Odenhall <dag.odenhall@gmail.com>
2011-04-17 15:46:38 +0200

    Get tests to pass on Python 3.1, possible Python bug?

--- attest/tests/hook.py
+++ attest/tests/hook.py
@@ -7,7 +7,6 @@ suite = Tests()
 @suite.test
 def eval():
     value = 1 + 1
-    valgen = (v for v in [value])
 
     samples = {
         'isinstance(value, int)': 'True',
@@ -15,9 +14,23 @@ def eval():
         'type(value).__name__': "'int'",
         'value == 5 - 3': '(2 == 2)',
         '{"value": value}': "{'value': 2}",
-        '[valgen.next() for _ in [value]] == [v for v in [value]]':
-            '([2] == [2])',
+        '[v for v in [value]]': '[2]',
     }
+
+    # FIXME Referencing a local inside a list comprehension fails on Python
+    # 3.1 for some reason, with a NameError saying *global* name not
+    # defined. Passing locals as globals works.
+
+    """
+    >>> eval("[f(v) for f in (str,float)]", globals(), dict(v=2))
+    Traceback (most recent call last):
+    File "<stdin>", line 1, in <module>
+    File "<string>", line 1, in <module>
+    File "<string>", line 1, in <listcomp>
+    NameError: global name 'v' is not defined
+    >>> eval("[f(v) for f in (str,float)]", dict(v=2), globals())
+    ['2', 2.0]
+    """
 
     for expr, result in samples.iteritems():
         ev = repr(ExpressionEvaluator(expr, globals(), locals()))
