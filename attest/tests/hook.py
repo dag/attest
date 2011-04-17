d6bff0a4e632f0bda9a143acede58c0765066ada
Dag Odenhall <dag.odenhall@gmail.com>
2011-04-17 15:08:38 +0200

    Fix tests for visit_Attribute on 2.5/PyPy

--- attest/tests/hook.py
+++ attest/tests/hook.py
@@ -12,7 +12,7 @@ def eval():
     samples = {
         'isinstance(value, int)': 'True',
         'value == int("2")': "(2 == 2)",
-        'value.denominator': '1',
+        'type(value).__name__': "'int'",
         'value == 5 - 3': '(2 == 2)',
         '{"value": value}': "{'value': 2}",
         '[valgen.next() for _ in [value]] == [v for v in [value]]':
