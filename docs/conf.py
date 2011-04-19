2f00425bc43710a84fff9298e30c3907fc7698b6
Dag Odenhall <dag.odenhall@gmail.com>
2011-04-19 23:41:18 +0200

    Devel docs in RTD default theme

--- docs/conf.py
+++ docs/conf.py
@@ -100,7 +100,7 @@ exclude_patterns = ['_build']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'attest'
+html_theme = 'attest' if 'dev' not in release else 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
