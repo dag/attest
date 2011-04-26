c9f1ee004de1eb22c891c9cc84133048d513ee1b
Dag Odenhall <dag.odenhall@gmail.com>
2011-04-26 17:11:52 +0200

    Utils for test scanning

--- attest/tests/utils.py
+++ attest/tests/utils.py
@@ -1,7 +1,10 @@
 from __future__ import with_statement
-from attest import Tests, assert_hook, utils, disable_imports
+from attest import Tests, assert_hook, utils, disable_imports, raises
+import attest
+from attest.utils import import_dotted_name
 
 suite = Tests()
+
 
 @suite.test
 def terminal_size():
@@ -16,3 +19,60 @@ def terminal_size():
         assert size == (80, 24)
         size = utils.get_terminal_size((1, 2))
         assert size == (1, 2)
+
+
+@suite.test
+def string_importing():
+    assert import_dotted_name('attest') is attest
+    assert import_dotted_name('attest.tests') is attest.tests
+    assert import_dotted_name('attest.utils') is utils
+    assert import_dotted_name('attest.utils:import_dotted_name') \
+           is import_dotted_name
+    assert import_dotted_name('attest.utils.import_dotted_name') \
+           is import_dotted_name
+
+
+    with raises(AttributeError):
+        import_dotted_name('attest._nil')
+
+    with raises(ImportError):
+        with disable_imports('attest'):
+            import_dotted_name('attest')
+
+
+@suite.test
+def iter_mods():
+    core = ['attest.' + mod for mod in
+            '''ast codegen collectors contexts deprecated hook __main__ reporters
+               run statistics utils'''.split()]
+    tests = ['attest.tests.' + mod for mod in
+              '''asserts classy collectors contexts hook _meta reporters
+                 utils'''.split()]
+
+    found = list(utils.deep_iter_modules('attest'))
+    expected = core + tests
+    assert set(expected) == set(found)
+    assert len(expected) == len(found)
+
+    found = list(utils.deep_iter_modules('attest.tests'))
+    expected = tests
+    assert set(expected) == set(found)
+    assert len(expected) == len(found)
+
+    found = list(utils.deep_iter_modules('attest.ast'))
+    assert found == ['attest.ast']
+
+    with raises(AttributeError):
+        list(utils.deep_iter_modules('attest._nil'))
+
+    with disable_imports('attest'):
+        with raises(ImportError):
+            list(utils.deep_iter_modules('attest'))
+
+
+@suite.test
+def get_members_recursively():
+    deepfunc = lambda x: getattr(x, '__name__', '').startswith('deep_')
+    found = list(utils.deep_get_members('attest', deepfunc))
+    expected = [utils.deep_get_members, utils.deep_iter_modules]
+    assert found == expected
--- attest/utils.py
+++ attest/utils.py
@@ -1,5 +1,7 @@
 import sys
 from array import array
+from pkgutil import iter_modules
+from inspect import getmembers
 
 
 def get_terminal_size(default=(80, 24)):
@@ -15,3 +17,59 @@ def get_terminal_size(default=(80, 24)):
         return default
     ary = array('h', fcntl.ioctl(sys.stdin, termios.TIOCGWINSZ, chr(0) * 8))
     return ary[1], ary[0]
+
+
+def import_dotted_name(name):
+    """Get an object by its "dotted name", a string representing its import
+    location. The last dot can also be a colon instead.
+
+    .. versionadded:: 0.6
+
+    """
+    name = str(name)
+    if ':' in name:
+        module, obj = name.split(':', 1)
+    elif '.' in name:
+        module, obj = name.rsplit('.', 1)
+    else:
+        return __import__(name)
+    mod = __import__(module, fromlist=[obj])
+    return getattr(mod, obj)
+
+
+def deep_iter_modules(name):
+    """Iterate over all modules under an import name. Like
+    :func:`~pkgutil.iter_modules` but recursive, and yielding the dotted
+    names of each module.
+
+    .. versionadded:: 0.6
+
+    """
+    mod = import_dotted_name(name)
+    if not hasattr(mod, '__path__'):
+        yield name
+        return
+    for _, name, _ in iter_modules(mod.__path__, name + '.'):
+        for name in deep_iter_modules(name):
+            yield name
+
+
+def deep_get_members(name, predicate=None, private=False):
+    """Get all top-level objects in all modules under `name` satisfying
+    `predicate` if provided, ignoring private modules and objects unless
+    `private` is true.
+
+    .. versionadded:: 0.6
+
+    """
+    seen = set()
+    for name in deep_iter_modules(name):
+        mod = import_dotted_name(name)
+        name = name.rsplit('.', 1)[1]
+        if not private and name.startswith('_'):
+            continue
+        for name, value in getmembers(mod, predicate):
+            if id(value) in seen or not private and name.startswith('_'):
+                continue
+            yield value
+            seen.add(id(value))
